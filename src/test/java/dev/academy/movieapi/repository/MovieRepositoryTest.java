2e0bed327bba6b11fe5ae93205f117a426f0f2bc
Caio Antonio <caiocichetti08@gmail.com>
2021-09-25 23:43:52 -0300

    test: added test for movie find by name feature

--- src/test/java/dev/academy/movieapi/repository/MovieRepositoryTest.java
+++ src/test/java/dev/academy/movieapi/repository/MovieRepositoryTest.java
@@ -1,10 +1,14 @@
 package dev.academy.movieapi.repository;
 
 import dev.academy.movieapi.domain.Movie;
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,14 +16,16 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @DisplayName("Tests for movie repository")
 class MovieRepositoryTest {
 
     @Autowired
     private MovieRepository movieRepository;
 
-    @DisplayName("should create movie when successful")
     @Test
+    @DisplayName("should create movie when successful")
     void shouldCreateOne() {
         var movieToBeSaved = createMovie();
         var movieSaved = this.movieRepository.save(movieToBeSaved);
@@ -30,8 +36,29 @@ class MovieRepositoryTest {
         assertThat(movieSaved.getCreatedOn()).isEqualTo(movieToBeSaved.getCreatedOn());
     }
 
-    @DisplayName("should update movie when successful")
     @Test
+    @DisplayName("should find by name when successful")
+    void shouldFindByName() {
+        var movieToBeSaved = createMovie();
+        var movieSaved = this.movieRepository.save(movieToBeSaved);
+        var name = movieSaved.getName();
+
+        var movies = this.movieRepository.findByName(name);
+
+        assertThat(movies).isNotEmpty();
+        assertThat(movies).contains(movieSaved);
+    }
+
+    @Test
+    @DisplayName("should return empty list when movie is not found")
+    void shouldReturnEmptyListWhenMovieIsNotFound() {
+        var movies = this.movieRepository.findByName("");
+
+        assertThat(movies).isEmpty();
+    }
+
+    @Test
+    @DisplayName("should update movie when successful")
     void shouldUpdateOne() {
         var movieToBeSaved = createMovie();
         var movieSaved = this.movieRepository.save(movieToBeSaved);
@@ -46,6 +73,19 @@ class MovieRepositoryTest {
         assertThat(movieUpdated.getName()).isEqualTo(movieSaved.getName());
         assertThat(movieUpdated.getCreatedOn()).isEqualTo(movieSaved.getCreatedOn());
         assertThat(movieUpdated.getUpdatedOn()).isEqualTo(movieSaved.getUpdatedOn());
+    }
+
+    @Test
+    @DisplayName("should delete movie when successful")
+    void shouldDeleteOne() {
+        var movieToBeSaved = createMovie();
+        var movieSaved = this.movieRepository.save(movieToBeSaved);
+
+        this.movieRepository.delete(movieSaved);
+
+        var movieId = this.movieRepository.findById(movieSaved.getId());
+
+        assertThat(movieId).isEmpty();
     }
 
     private Movie createMovie() {
