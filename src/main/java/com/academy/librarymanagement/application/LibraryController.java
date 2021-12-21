508b7ad653f3f46a109f65cb5d6c59415f0819fa
Caio Roberto <caio.roberto@sensedia.com>
2021-12-21 18:42:09 -0300

    fix: removed unused dependency

--- src/main/java/com/academy/librarymanagement/application/LibraryController.java
+++ src/main/java/com/academy/librarymanagement/application/LibraryController.java
@@ -10,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("library-management/v1/books")
