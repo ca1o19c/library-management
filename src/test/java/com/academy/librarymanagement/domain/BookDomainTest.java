e0e91a151a95ed57d7cd6b53a5247b33166e12db
Caio Antonio <caio.antonio.c@outlook.com>
2021-12-21 21:32:31 -0300

    test: created domain test

--- src/test/java/com/academy/librarymanagement/domain/BookDomainTest.java
+++ src/test/java/com/academy/librarymanagement/domain/BookDomainTest.java
@@ -1,19 +1,35 @@
 package com.academy.librarymanagement.domain;
 
-import org.junit.Assert;
+import com.academy.librarymanagement.mock.BookMock;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BookDomainTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DisplayName("Book Test")
+class BookDomainTest {
+
+    private final Book book = new Book();
+
     @Test
-    void teste1() {
-        Assert.assertTrue(true);
+    void shouldRetrieveCreatedDateTime() {
+        var createdDateTime = book.created();
+
+        assertNotNull(createdDateTime.getCreatedOn());
     }
+
     @Test
-    void teste2() {
-        Assert.assertTrue(true);
-    }
-    @Test
-    void teste3() {
-        Assert.assertFalse(true);
+    void shouldRetrieveUpdatedDateTimeAndData() {
+        var bookMock = BookMock.aMock();
+
+        var updatedDateTime = book.updated(bookMock);
+
+        assertNotNull(updatedDateTime.getUpdatedOn());
+
+        assertEquals("Harry Potter", bookMock.getTitle());
+        assertEquals("Rocco", bookMock.getPublisher());
+        assertEquals("https://i.imgur.com/UH3IPXw.jpg", bookMock.getImage());
+        assertEquals("JK Rowling", bookMock.getWriters().get(0));
     }
 }
