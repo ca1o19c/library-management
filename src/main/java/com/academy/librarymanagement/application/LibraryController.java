17f1c539b1dfb6a5c83425810f918d41f5b6d3fa
Caio Antonio <caio.antonio.c@outlook.com>
2021-12-24 10:14:24 -0300

    feat: implements design patterns

--- src/main/java/com/academy/librarymanagement/application/LibraryController.java
+++ src/main/java/com/academy/librarymanagement/application/LibraryController.java
@@ -1,15 +1,14 @@
 package com.academy.librarymanagement.application;
 
-import com.academy.librarymanagement.domain.Book;
 import com.academy.librarymanagement.domain.BookDto;
 import com.academy.librarymanagement.infra.crosscutting.LibraryActions;
 import com.academy.librarymanagement.services.LibraryService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("library-management/v1/books")
@@ -18,16 +17,21 @@ public class LibraryController {
     private final LibraryService libraryService;
     private final LibraryActions libraryActions;
 
-
     public LibraryController(final LibraryService libraryService, final LibraryActions libraryActions) {
         this.libraryService = libraryService;
         this.libraryActions = libraryActions;
     }
 
     @GetMapping
-    public ResponseEntity<Page<Book>> findAll(Pageable pageable) {
-        var body = libraryService.findAll(pageable);
-        return ResponseEntity.ok(body);
+    public ResponseEntity<List<BookDto>> findAll() {
+        var body = libraryService.findAll();
+
+        var payload = body
+                .stream()
+                .map(BookDto::from)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(payload);
     }
 
     @PostMapping
@@ -38,7 +42,17 @@ public class LibraryController {
     }
 
     @GetMapping(path = "{id}")
-    public ResponseEntity<Book> findOne(@PathVariable String id) {
-        return ResponseEntity.ok(libraryService.findOne(id));
+    public ResponseEntity<BookDto> findOne(@PathVariable String id) {
+        var entity = libraryService.findOne(id);
+        var payload = BookDto.from(entity);
+
+        return ResponseEntity.ok(payload);
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<BookDto> findByTitle(@RequestParam String title) {
+        var entity = libraryService.findByTitle(title);
+        var payload = BookDto.from(entity);
+        return ResponseEntity.ok(payload);
     }
 }
--- src/main/java/com/academy/librarymanagement/infra/data/LibraryRepository.java
+++ src/main/java/com/academy/librarymanagement/infra/data/LibraryRepository.java
@@ -4,6 +4,9 @@ import com.academy.librarymanagement.domain.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LibraryRepository extends MongoRepository<Book, String> {
+    Optional<Book> findByTitle(String title);
 }
