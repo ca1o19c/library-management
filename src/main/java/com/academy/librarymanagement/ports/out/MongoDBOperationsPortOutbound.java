b1b146db4a1160cb5fe12c40b8cc54c3eb5df133
Caio Antonio <caio.antonio.c@outlook.com>
2022-03-27 10:37:52 -0300

    chore: organized project folders

---
+++ src/main/java/com/academy/librarymanagement/ports/out/MongoDBOperationsPortOutbound.java
@@ -0,0 +1,15 @@
+package com.academy.librarymanagement.ports.out;
+
+import com.academy.librarymanagement.adapters.out.ResearchedBook;
+import com.academy.librarymanagement.domain.Book;
+import com.academy.librarymanagement.domain.BookSearch;
+import com.academy.librarymanagement.domain.FilteredBook;
+
+import java.util.Optional;
+
+public interface MongoDBOperationsPortOutbound {
+    ResearchedBook findAll(BookSearch search);
+    void save(Book book);
+    Optional<com.academy.librarymanagement.adapters.out.Book> findOne(String id);
+    Optional<com.academy.librarymanagement.adapters.out.Book> deleteOne(String id);
+}
