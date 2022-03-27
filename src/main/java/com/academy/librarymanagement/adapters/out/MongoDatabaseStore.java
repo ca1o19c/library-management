3cb0e8951b12d8ac01808e67cbff1e61d923592c
Caio Antonio <caio.antonio.c@outlook.com>
2022-03-27 10:38:21 -0300

    chore: organized project folders

--- src/main/java/com/academy/librarymanagement/adapters/out/MongoDatabaseStore.java
+++ src/main/java/com/academy/librarymanagement/adapters/out/MongoDatabaseStore.java
@@ -1,118 +1,77 @@
 package com.academy.librarymanagement.adapters.out;
 
-import com.academy.librarymanagement.domain.SortType;
-import com.academy.librarymanagement.ports.out.MongoDatabaseStoreOutbound;
+import com.academy.librarymanagement.adapters.config.exception.BookNotFoundException;
+import com.academy.librarymanagement.domain.BookSearch;
+import com.academy.librarymanagement.domain.FilteredBook;
+import com.academy.librarymanagement.ports.out.MongoDatabaseStorePortOutbound;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import static java.util.Objects.isNull;
-import static org.apache.commons.lang3.ObjectUtils.isEmpty;
-
-@Service
-class MongoDatabaseStore implements MongoDatabaseStoreOutbound {
-
-    private static final String CREATED_ON_PROPERTY = "createdOn";
-    private static final String TITLE_PROPERTY = "title";
-    private static final String PUBLISHER_PROPERTY = "publisher";
-    private static final String ID_PROPERTY = "id";
+@Component
+class MongoDatabaseStore implements MongoDatabaseStorePortOutbound {
 
     @Autowired
-    private MongoTemplate mongoTemplate;
+    private MongoDBOperations mongoDBOperations;
 
     @Override
-    public ResearchedBook findAll(com.academy.librarymanagement.domain.BookSearch search) {
-        Query query = this.buildQuery(search);
+    public FilteredBook findAll(BookSearch search) {
+        ResearchedBook books = mongoDBOperations.findAll(search);
 
-        int page = search.getPage();
-
-        int limit = search.getLimit();
-
-        Sort.Direction direction = Sort.Direction.fromString(isEmpty(search.getSortType()) ? SortType.ASC.getValue() : search.getSortType().getValue());
-
-        int total = Math.toIntExact(mongoTemplate.count(query, Book.class));
-
-        query.with(PageRequest.of(page, limit, Sort.by(direction, CREATED_ON_PROPERTY)));
-
-        List<Book> books = mongoTemplate.find(query, Book.class);
-
-        return new ResearchedBook(total, books);
+        return buildBookAggregate(books);
     }
 
     @Override
     public void save(com.academy.librarymanagement.domain.Book book) {
-        Book bookDocument = new Book();
-
-        bookDocument.setId(book.getId());
-        bookDocument.setTitle(book.getTitle());
-        bookDocument.setImage(book.getImage());
-        bookDocument.setPublisher(book.getPublisher());
-        bookDocument.setWriters(book.getWriters());
-        bookDocument.created();
-
-        mongoTemplate.save(bookDocument);
+        mongoDBOperations.save(book);
     }
 
     @Override
-    public Optional<Book> findOne(String id) {
-        Query query = new Query();
+    public com.academy.librarymanagement.domain.Book findOne(String id) {
+        com.academy.librarymanagement.adapters.out.Book book = mongoDBOperations.findOne(id).orElseThrow(() -> new BookNotFoundException("Book with id " + id + " not found"));
 
-        query.addCriteria(Criteria.where(ID_PROPERTY).is(id));
-
-        return Optional.ofNullable(mongoTemplate.findOne(query, Book.class));
+        return com.academy.librarymanagement.domain.Book.builder()
+                .withId(book.getId())
+                .withTitle(book.getTitle())
+                .withImage(book.getImage())
+                .withPublisher(book.getPublisher())
+                .withWriters(book.getWriters())
+                .withCreatedOn(book.getCreatedOn())
+                .withUpdatedOn(book.getUpdatedOn())
+                .build();
     }
 
     @Override
-    public Optional<Book> deleteOne(String id) {
-        Query query = new Query();
-
-        query.addCriteria(Criteria.where(ID_PROPERTY).is(id));
-
-        return Optional.ofNullable(mongoTemplate.findAndRemove(query, Book.class));
+    public void deleteOne(String id) {
+        mongoDBOperations.deleteOne(id).orElseThrow(() -> new BookNotFoundException("Book with id " + id + " not found"));
     }
 
-    private Query buildQuery(com.academy.librarymanagement.domain.BookSearch search) {
-        Query query = new Query();
+    private FilteredBook buildBookAggregate(ResearchedBook books) {
 
-        Instant initialDate = toStartOfDay(search.getInitialDate());
-        Instant finalDate = toEndOfDay(search.getFinalDate());
+        com.academy.librarymanagement.domain.Book.Builder bookBuilder = com.academy.librarymanagement.domain.Book.builder();
 
-        if (!isNull(initialDate) && !isNull(finalDate))
-            query.addCriteria(Criteria.where(CREATED_ON_PROPERTY).gte(initialDate).lte(finalDate));
+        List<com.academy.librarymanagement.domain.Book> listBooks = new ArrayList<>();
 
-        if (!isNull(initialDate) && isNull(finalDate))
-            query.addCriteria(Criteria.where(CREATED_ON_PROPERTY).gte(initialDate));
+        books.getResult().forEach(book -> {
+                    bookBuilder
+                            .withId(book.getId())
+                            .withImage(book.getImage())
+                            .withPublisher(book.getPublisher())
+                            .withTitle(book.getTitle())
+                            .withWriters(book.getWriters())
+                            .withCreatedOn(book.getCreatedOn())
+                            .withUpdatedOn(book.getUpdatedOn());
 
-        if (!isNull(finalDate) && isNull(initialDate))
-            query.addCriteria(Criteria.where(CREATED_ON_PROPERTY).lte(finalDate));
+                    listBooks.add(bookBuilder.build());
+                }
+        );
 
-        String title = search.getTitle();
-
-        if (!isNull(title)) query.addCriteria(Criteria.where(TITLE_PROPERTY).is(title));
-
-        String publisher = search.getPublisher();
-
-        if (!isNull(publisher)) query.addCriteria(Criteria.where(PUBLISHER_PROPERTY).is(publisher));
-
-        return query;
-    }
-
-    private Instant toStartOfDay(LocalDate localDate) {
-        return localDate == null ? null : localDate.atStartOfDay(ZoneId.of("UTC")).toInstant();
-    }
-
-    private Instant toEndOfDay(LocalDate localDate) {
-        return localDate == null ? null : localDate.atTime(LocalTime.MAX).atZone(ZoneId.of("UTC")).toInstant();
+        return FilteredBook.builder()
+                .withBooks(listBooks)
+                .withTotal(books.getTotal())
+                .build();
     }
 }
